26c5793bdd94d61c02ba7c498f390f14ca0bc52e
Ayush Jain <jain.ayushkumar0@gmail.com>
2020-05-19 17:23:24 +0200

    Added CameraCalibration folder and LaneDetectionProjectReport

--- CameraCalibration/Code/UndistortAndTransformImage.py
+++ CameraCalibration/Code/UndistortAndTransformImage.py
@@ -12,6 +12,7 @@ dist = dist_pickle["dist"]
 
 # Read in an image
 img = cv2.imread('test_image2.png')
+print (img.shape)
 nx = 8 # the number of inside corners in x
 ny = 6 # the number of inside corners in y
 
@@ -26,7 +27,7 @@ def corners_unwarp(img, nx, ny, mtx, dist):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     # 3) Find the chessboard corners
     ret, corners = cv2.findChessboardCorners(gray, (nx,ny), None)
-    
+    print (corners)
     if ret == True:
     # 4) If corners found: 
             # a) draw corners
@@ -37,12 +38,14 @@ def corners_unwarp(img, nx, ny, mtx, dist):
             offset = 100 # offset for dst points
             # Grab the image shape
             img_size = (gray.shape[1], gray.shape[0])
+            print (img_size)
             # b) define 4 source points src = np.float32([[,],[,],[,],[,]])
                  #Note: you could pick any four of the detected corners 
                  # as long as those four corners define a rectangle
                  # One especially smart way to do this would be to use four well-chosen
                  # corners that were automatically detected during the undistortion steps
                  # We recommend using the automatic detection of corners in your code
+            # 4 corners of the biggest triangle
             src = np.float32([corners[0], corners[nx-1], corners[-1], corners[-nx]])
             # c) define 4 destination points dst = np.float32([[,],[,],[,],[,]])
             dst = np.float32([[offset, offset], [img_size[0]-offset, offset], 
