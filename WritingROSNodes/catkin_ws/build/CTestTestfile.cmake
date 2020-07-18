5998a0a0231387e5344c4cb7d3c2f4cfb010ac38
Ayush Jain <jain.ayushkumar0@gmail.com>
2020-07-18 14:19:38 +0200

    Arranged

--- WritingROSNodes/catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jainayushkumar0/Documents/Courses/SelfDrivingCarNanoDegree/WritingROSNodes/catkin_ws/src
-# Build directory: /home/jainayushkumar0/Documents/Courses/SelfDrivingCarNanoDegree/WritingROSNodes/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("simple_arm")
